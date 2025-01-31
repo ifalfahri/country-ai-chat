b78ff76e8199cd61ab1d4e6df2096b41a5c291a4
ifalfahri <ifaltampan69@gmail.com>
2025-01-31 14:04:53 +0800

    fix: flag emoji doesnt show on chromium browser on windows

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     margin: 0;
     padding: 0;
-    font-family: 'Poppins', serif;
+    font-family: 'Twemoji Country Flags', 'Poppins', serif;
   }
 
   body {
