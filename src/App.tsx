cd4921bcdfd3616bc3a8c12712346f117ca4c0dc
ifalfahri <ifaltampan69@gmail.com>
2025-02-02 17:39:32 +0800

    feat: implement routing and protected routes with authentication context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import { HomePage } from "./pages/HomePage";
+import { LoginPage } from "./pages/LoginPage";
 import { GlobalStyle } from "./styles/global";
 import { theme } from "./styles/theme";
 import { ChatProvider } from "./context/ChatContext";
+import { AuthProvider } from "./context/AuthContext";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <ChatProvider>
-      <GlobalStyle />
-      <HomePage />
-      </ChatProvider>
-    </ThemeProvider>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <ChatProvider>
+            <GlobalStyle />
+            <BrowserRouter>
+              <Routes>
+                <Route path="/login" element={<LoginPage />} />
+                <Route
+                  path="/"
+                  element={
+                    <ProtectedRoute>
+                      <>
+                        <Navbar />
+                        <HomePage />
+                      </>
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="*" element={<Navigate to="/" replace />} />
+              </Routes>
+            </BrowserRouter>
+          </ChatProvider>
+        </AuthProvider>
+      </ThemeProvider>
+    </GoogleOAuthProvider>
   );
 }
 
