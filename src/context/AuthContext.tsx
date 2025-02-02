591d7faacfa59cf5b5c5797f5ba62800004b7ab5
ifalfahri <ifaltampan69@gmail.com>
2025-02-02 18:15:18 +0800

    fix: add eslint directive to prevent warnings for useAuth export

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -76,4 +76,5 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => useContext(AuthContext);
